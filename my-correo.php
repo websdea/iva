15c90ac657d4894a6ce40f9146134d1ed5d8e551
websdea <70303178+websdea@users.noreply.github.com>
2020-08-28 23:06:22 -0500

    Add files via upload

---
+++ my-correo.php
@@ -0,0 +1,17 @@
+<?php
+   $destinatario = 'meraro2005@gmail.com';
+   //esto es al correo que se enviara el mensaje
+   $nombre = $_POST['nombre'];
+   $telefono = $_POST['telefono'];
+   $mensaje = $_POST['mensaje'];
+   $email = $_POST['email'];
+
+   $header = "Enviado desde la Pagina de Iglesia V.A.";
+   $mensajecompleto = $mensaje . "\nAtentamente: " . $nombre 
+
+   mail($destinatario, $telefono, $mensaje, $header);
+
+   echo "<script>alert('correo enviado exitosamente')</script>";
+   echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
