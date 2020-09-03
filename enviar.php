6270e710654e7b54400c04da95d53f1589e47e1b
websdea <70303178+websdea@users.noreply.github.com>
2020-09-03 15:00:03 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -9,3 +9,26 @@
    mail($destino,"Contacto", $contenido);
    header("Location:gracias.html");   
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+		<meta content="text/html; charset=utf-" http-equiv="Content-Type" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<title>Gracias 1</title>
+		<link rel="stylesheet" href="css/contacto.css" />
+
+		<!--CSS-->
+		<link rel="stylesheet" href="css/global.css" />
+		<link rel="stylesheet" href="css/font-awesome.min.css" />
+
+		<!--JS-->
+</head>
+<body>
+	<header class="cls-gracias">
+		<h1>GRACIAS POR ESCRIBIRNOS</h1>
+	</header>
+</body>
+</html>
