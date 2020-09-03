635a904496312ff68dc75b61a519f1245e013a78
websdea <70303178+websdea@users.noreply.github.com>
2020-09-03 14:18:18 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+   $destino = "meraro2005@gmail.com";
+   $nombre = $_POST["nombre"];
+   $correo = $_POST["correo"];
+   $telefono = $_POST["telefono"];
+   $mensaje = $_POST["mensaje"];
+   
+   $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+   mail($destino,"Contacto", $contenido);
+   header("Location:gracias.html");   
+?>
